bd223e91e202943f4fe4bd093f28ecb4c5c518ff
ano <anoidgit@users.noreply.github.com>
2017-06-15 10:15:05 +0800

    Create main.php

--- templates/main.php
+++ templates/main.php
@@ -51,7 +51,7 @@ html body {
 <div id="container">
 
 	<div style="text-align:center;">
-		<h1 style="color:blue;">PHP-Proxy</h1>
+		<h1 style="color:blue;">ano-Proxy</h1>
 	</div>
 	
 	<?php if(isset($error_msg)){ ?>
